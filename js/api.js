3f5d3ebe56eea275051a48cad275013b2cf84809
leo <leo.sun@berkeley.edu>
2018-07-12 13:25:29 -0700

    send email now sends an email

---
+++ js/api.js
@@ -0,0 +1,7 @@
+var BASE_URL = 'https://backend.grailhq.com/api/';
+
+var API = {
+    CREATE_ACCOUNT: BASE_URL + 'create_account/',
+}
+
+export default API;
--- js/email.js
+++ js/email.js
@@ -1,4 +1,18 @@
+var BASE_URL = 'https://backend.grailhq.com/api/';
+
+var API = {
+    CREATE_ACCOUNT: BASE_URL + 'create_account/',
+}
+
 function sendEmail (){
     document.getElementById('install').style.display = "flex";
     document.getElementById('email').style.display = "none";
+
+    let email = document.getElementById('email-data').value;
+    let request = new XMLHttpRequest();
+    let api = API.CREATE_ACCOUNT;
+
+    request.open('POST', api, true);
+    request.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
+    request.send('email='+email);
 }
